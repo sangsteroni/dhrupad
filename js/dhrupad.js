b917d8275eabce299e6ec1d68616901f1bfd880f
Sangeetha Narasimhan <nsg@twimo.in>
2014-06-28 01:30:31 +0530

    Working Parse backend for RSVP

---
+++ js/dhrupad.js
@@ -0,0 +1,76 @@
+var dhApp = angular.module('dhrupad', []);
+
+dhApp.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top-50}, "slow");
+      });
+    }
+  }
+});
+
+
+function RSVPCtrl($scope, $http) {
+  /*
+   * On startup...
+   */
+  $scope.rsvps = [];
+  Parse.initialize("fUijf067YybdiQB7Io13VP66kDjkMQBASOXx16Yo", "338NN4cSdTPNZX3gbJ9YVDGoLxsDnEVnPev83w06");
+
+  // Get all RSVPs
+  $scope.getRsvps = function() {
+    $http({
+	method : 'GET', 
+	url : 'https://api.parse.com/1/classes/Rsvp', 
+	headers: { 'X-Parse-Application-Id':'fUijf067YybdiQB7Io13VP66kDjkMQBASOXx16Yo', 'X-Parse-REST-API-Key':'hezqkxVrcfc5BuaCCqkcOw4lNpmVSmmqxWbbFOE6'}}).
+            success(function(data, status) {
+                $scope.rsvps = data;
+            }).
+            error(function(data, status) {
+                alert("Error");
+            });
+  };
+
+
+  $scope.addRsvp = function() {
+    $http({
+	method : 'POST',
+	url : 'https://api.parse.com/1/classes/Rsvp',
+	data : JSON.stringify({name: $scope.name, attendees: $scope.attendees, invitedby: $scope.invitedby}),
+        headers: { 'X-Parse-Application-Id':'fUijf067YybdiQB7Io13VP66kDjkMQBASOXx16Yo', 
+		   'X-Parse-REST-API-Key':'hezqkxVrcfc5BuaCCqkcOw4lNpmVSmmqxWbbFOE6'}}).
+            success(function(data, status) {
+		$scope.getRsvps();
+		$scope.message = "Thank you. Your RSVP has been recorded. Please make the payment to "+$scope.invitedby+" to confirm your spot.";
+                alert($scope.message);
+		$scope.name="";
+		$scope.attendees="";
+		$scope.invitedby="";
+            }).
+            error(function(data, status) {
+		$scope.message = "Sorry, there seems to be some problem. We couldn't record your RSVP. Please contact "+$scope.invitedby+" to confirm your presence and to make the payment.";
+                alert("Error");
+            });
+  };
+
+
+  // Count total guests
+  $scope.total_guests = function() {
+    var count = 0;
+    angular.forEach($scope.rsvps.results, function(rsvp) {
+      count += rsvp.attendees;
+    });
+    return count;
+  };
+
+
+}
